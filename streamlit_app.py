c9c1e46cb17fc5a523f427213c229521beee1034
Faith Zhang <fz2*@rice.edu>
2024-07-08 20:39:30 -0500

    more example modificaitons

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,13 @@
 import streamlit as st
 
 def main():
-    st.title('My Streamlit App')
+    st.title('AKI Prediction Dashboard')
     st.write("Hello, world!")
+
+    st.sidebar.header('User Input')
+    slider_value = st.sidebar.slider('Select a range', 0, 100, (25, 75))
+    text_input = st.sidebar.text_input('Enter your name', 'John Doe')
+    checkbox = st.sidebar.checkbox('Show data')
 
 if __name__ == '__main__':
     main()
