fdb58f49aaa705e8cbf5e3e18954f7f910b288f5
Faith Zhang <fz2*@rice.edu>
2024-07-08 20:45:31 -0500

    allow csv file upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,29 @@ def main():
     st.title('AKI Prediction Dashboard')
     st.write("For Patient No:", hadm_id)
 
+    uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
+
+    # Check if file was uploaded and process if so
+    if uploaded_file is not None:
+        # Read CSV file
+        df = pd.read_csv(uploaded_file)
+        
+        # Display the dataframe
+        st.write("### Uploaded DataFrame:")
+        st.write(df)
+
+
+
+
+
+
     text_input = st.sidebar.text_input('Clinician Name', 'John Doe')
     st.sidebar.header('User Input')
     slider_value = st.sidebar.slider('Select a range', 0, 100, (25, 75))
     
     checkbox = st.sidebar.checkbox('Show data')
 
-        # Displaying content based on user inputs
+    # Displaying content based on user inputs
     st.write(f'Hello, {text_input}!')
     
     if checkbox:
